e24638468e2cae22dd683593ba6a985a542736bc
mizutanimasaya <mizu01181996@yahoo.co.jp>
2026-09-18 14:56:43 +0900

    fix:水谷　Case01,02のテストコード実装。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -2,6 +2,8 @@ package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +11,11 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能①
@@ -18,6 +25,8 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース01 ログイン画面への遷移")
 public class Case01 {
+
+	private int port = 8080;
 
 	/** 前処理 */
 	@BeforeAll
@@ -35,7 +44,15 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		WebDriverUtils.goTo("http://localhost:" + port + "/lms");
+
+		final WebDriverWait wait = new WebDriverWait(WebDriverUtils.webDriver, Duration.ofSeconds(60));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("form-group")));
+
+		WebDriverUtils.getEvidence(new Object() {
+		});
+
 	}
 
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -2,6 +2,8 @@ package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +11,12 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能①
@@ -18,6 +26,8 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース02 受講生 ログイン 認証失敗")
 public class Case02 {
+
+	private int port = 8080;
 
 	/** 前処理 */
 	@BeforeAll
@@ -35,14 +45,82 @@ public class Case02 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		WebDriverUtils.goTo("http://localhost:" + port + "/lms");
+
+		final WebDriverWait wait = new WebDriverWait(WebDriverUtils.webDriver, Duration.ofSeconds(60));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("form-group")));
+
+		WebDriverUtils.getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		final WebElement loginId = WebDriverUtils.webDriver.findElement(By.id("loginId"));
+		loginId.clear();
+		loginId.sendKeys("mizutani");
+
+		final WebElement loginPass = WebDriverUtils.webDriver.findElement(By.id("password"));
+		loginPass.clear();
+		loginPass.sendKeys("mizutani");
+
+		final WebElement loginButton = WebDriverUtils.webDriver
+				.findElement(By.cssSelector("input[value='ログイン']"));
+		loginButton.click();
+
+		final WebDriverWait wait = new WebDriverWait(WebDriverUtils.webDriver, Duration.ofSeconds(60));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("form-group")));
+
+		WebDriverUtils.getEvidence(new Object() {
+		});
+	}
+
+	@Test
+	@Order(3)
+	@DisplayName("テスト03 IDを未入力でログイン")
+	void test03() {
+
+		final WebElement loginId = WebDriverUtils.webDriver.findElement(By.id("loginId"));
+		loginId.clear();
+		loginId.sendKeys("");
+
+		final WebElement loginPass = WebDriverUtils.webDriver.findElement(By.id("password"));
+		loginPass.clear();
+		loginPass.sendKeys("StudentAA031");
+
+		final WebElement loginButton = WebDriverUtils.webDriver
+				.findElement(By.cssSelector("input[value='ログイン']"));
+		loginButton.click();
+
+		final WebDriverWait wait = new WebDriverWait(WebDriverUtils.webDriver, Duration.ofSeconds(60));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("form-group")));
+
+		WebDriverUtils.getEvidence(new Object() {
+		});
+	}
+
+	@Test
+	@Order(4)
+	@DisplayName("テスト04 PASSを未入力でログイン")
+	void test04() {
+
+		final WebElement loginId = WebDriverUtils.webDriver.findElement(By.id("loginId"));
+		loginId.clear();
+		loginId.sendKeys("mizutani");
+
+		final WebElement loginButton = WebDriverUtils.webDriver
+				.findElement(By.cssSelector("input[value='ログイン']"));
+		loginButton.click();
+
+		final WebDriverWait wait = new WebDriverWait(WebDriverUtils.webDriver, Duration.ofSeconds(60));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("form-group")));
+
+		WebDriverUtils.getEvidence(new Object() {
+		});
 	}
 
 }
